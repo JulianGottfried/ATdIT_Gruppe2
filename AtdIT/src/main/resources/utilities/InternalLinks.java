b692f3b08fbed82fdd4a1392a1297014a41f8895
Marvin34565 <weitz_marvin@web.de>
2021-05-11 08:38:51 +0200

    fixed internal link2image

--- AtdIT/src/main/resources/utilities/InternalLinks.java
+++ AtdIT/src/main/resources/utilities/InternalLinks.java
@@ -1,7 +1,7 @@
 package main.resources.utilities;
 
 public class InternalLinks {
-    private static String link2Images = "src/main/resources/images/";
+    private static String link2Images = "AtdIT/src/main/resources/images/";
     private static String link2bundles = "main/resources/i18n/";
 
     private InternalLinks() {
