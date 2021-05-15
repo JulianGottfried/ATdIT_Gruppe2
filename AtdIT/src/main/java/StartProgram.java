b961ba73406078e92febb5c76509f699831b6d05
Marvin34565 <weitz_marvin@web.de>
2021-05-15 14:23:40 +0200

    Finalized Test for join

--- AtdIT/src/main/java/StartProgram.java
+++ AtdIT/src/main/java/StartProgram.java
@@ -1,22 +1,10 @@
 package main.java;
 
-import java.util.Date;
-
 import main.java.controller.handler.ScreenHandler;
-import main.java.persistence.databaseManager.DatabaseManager;
-import main.java.persistence.databaseTables.Addresses;
-import main.java.persistence.databaseTables.ChangesOfAddresses;
-import main.java.persistence.databaseTables.Persons;
 
 public class StartProgram {
 
     public static void main(String[] args) {
-        // new ScreenHandler();
-    	
-    	DatabaseManager dbm = new DatabaseManager();
-    	
-    	// moin meister
-    	ChangesOfAddresses x = new ChangesOfAddresses();
-    	dbm.writeToDatabase(x);
+        new ScreenHandler();
     }
 }
