21db447ae3583844783cf98efa9e4234b45240c1
Peter <53820878+weilichsoheisse@users.noreply.github.com>
2021-05-25 16:39:10 +0200

    code

---
+++ ATdIT/AtdIT/src/main/java/StartProgram.java
@@ -0,0 +1,16 @@
+package main.java;
+
+import main.java.controller.handler.ScreenHandler;
+/**
+ * Creates a new ScreenHandler and starts the program.
+ * 
+ * @author weilichsoheisse
+ * @version 17.05.2021
+ *
+ */
+public class StartProgram {
+
+	public static void main(String[] args) {
+		new ScreenHandler();
+    }
+}
